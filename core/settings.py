e52e7b090982417f2a312bb4a5f0f66a5922ed43
Miguel-EMC <eduardomuzo123456@gmail.com>
2024-07-15 10:19:20 -0500

    Update

--- core/settings.py
+++ core/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -139,10 +139,13 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-import os
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
+#BASE_DIR=os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+MEDIA_URLS ='/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # Default primary key field type
+
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
